51a593ad01bbe1a2a190e24f8350ff059deca015
bingcool <2437667702@qq.com>
2019-02-24 21:31:35 +0800

    封装supervisord的控制方法

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,13 +1,17 @@
 <?php
 namespace app\index\controller;
 
+use think\Controller;
 use think\facade\Config;
 
-class Index
+class Index extends Controller
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V5.1<br/><span style="font-size:30px">12载初心不改（2006-2018） - 你值得信赖的PHP框架</span></p></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="eab4b9f840753f8e7"></think>';
+        $name = "bingcool";
+        //var_dump($name);
+        $this->assign('name', 'thinkphp');
+        return $this->fetch();
     }
 
     public function hello($name = 'ThinkPHP5')
@@ -37,7 +41,7 @@ class Index
         }
     }
 
-    public function superctl()
+    public function getAllServerStatus()
     {
         
         $xmlConfs = Config::get('xmlrpc.supervisor_servers');
@@ -51,6 +55,6 @@ class Index
             $result[$server] = $response;
         }
 
-        return json($result);
+        return $result;
     }
 }
---
+++ application/service/supervisord/supervisorctl.php
@@ -0,0 +1,140 @@
+<?php
+namespace app\service\supervisord;
+
+use think\facade\Config;
+use app\common\xmlrpc\XmlRpc;
+
+class supervisorctl {
+
+    protected $supervisord_conf = [];
+
+    protected $xmlrpc;
+
+    /**
+     * supervisorctl constructor
+     */
+    public function __construct() {
+        $xmlConfs = Config::get('xmlrpc.supervisor_servers');
+        $this->supervisord_conf = $xmlConfs;
+        $this->xmlrpc = new XmlRpc();
+    }
+
+    /**
+     * startProcess 启动一个进程
+     * @param $process_name
+     * @param bool $wait
+     */
+    public function startProcess($server, $process_name, $wait = true) {
+        $this->xmlrpc->_request($server,'startProcess',[$process_name, $wait]);
+    }
+
+    /**
+     * startAllProcesses 启动服务下的所有进程
+     * @param $server
+     * @param bool $wait
+     * @throws \Exception
+     */
+    public function startAllProcesses($server, $wait = true) {
+        $this->xmlrpc->_request($server,'startAllProcesses',[$wait]);
+    }
+
+    /**
+     * startProcessGroup 启动某个分组下的所有进程
+     * @param $server
+     * @param $group_name
+     * @param bool $wait
+     * @throws \Exception
+     */
+    public function startProcessGroup($server, $group_name, $wait = true) {
+        $this->xmlrpc->_request($server,'startProcessGroup',[$group_name, $wait]);
+    }
+
+    /**
+     * stopProcess 停止服务的某个进程
+     * @param $server
+     * @param $process_name
+     * @param bool $wait
+     * @throws \Exception
+     */
+    public function stopProcess($server, $process_name, $wait = true) {
+        $this->xmlrpc->_request($server,'stopProcess',[$process_name, $wait]);
+    }
+
+    /**
+     * stopAllProcesses 停止服务下所有进程
+     * @param $server
+     * @param bool $wait
+     * @throws \Exception
+     */
+    public function stopAllProcesses($server, $wait = true) {
+        $this->xmlrpc->_request($server,'stopAllProcesses', [$wait]);
+    }
+
+    /**
+     * restartProcess 重启某个进程
+     * @param $server
+     * @param $process_name
+     * @param bool $wait
+     */
+    public function restartProcess($server, $process_name, $wait = true) {
+        $this->stopProcess($server, $process_name, $wait);
+        sleep(3);
+        $this->startProcess($server, $process_name, $wait);
+    }
+
+    /**
+     * restartAllProcess 重启所有的进程
+     * @param $server
+     * @param bool $wait
+     * @throws \Exception
+     */
+    public function restartAllProcess($server, $wait = true) {
+        $this->stopAllProcesses($server, $wait);
+        sleep(3);
+        $this->startAllProcesses($server, $wait);
+    }
+
+    /**
+     * getProcessInfo 获取某个进程的信息
+     * @param $server
+     * @param $process_name
+     * @return string
+     * @throws \Exception
+     */
+    public function getProcessInfo($server, $process_name) {
+        return $this->xmlrpc->_request($server, 'getProcessInfo', [$process_name]);
+    }
+
+    /**
+     * getAllProcessInfo 获取所有进程信息
+     * @param $server
+     * @param $process_name
+     * @return string
+     * @throws \Exception
+     */
+    public function getAllProcessInfo($server, $process_name) {
+        return $this->xmlrpc->_request($server, 'getAllProcessInfo', []);
+    }
+
+    /**
+     * clearProcessLogs 清空打印出的错误日志
+     * @param $server
+     * @param $process_name
+     * @throws \Exception
+     */
+    public function clearProcessLogs($server, $process_name) {
+        $this->xmlrpc->_request($server, 'clearProcessLogs', [$process_name]);
+    }
+
+    /**
+     * getSupervisorVersion 获取supervisord version
+     * @param $server
+     * @return string
+     * @throws \Exception
+     */
+    public function getSupervisorVersion($server) {
+        return $this->xmlrpc->_request($server, 'getSupervisorVersion');
+    }
+
+
+}
